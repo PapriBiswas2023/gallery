b7f77a725ae34727d315096e78fb4afd4900d85a
Papri Biswas <134720766+PapriBiswas2023@users.noreply.github.com>
2025-09-04 19:06:22 +0530

    otp-login

--- database/send-otp.php
+++ database/send-otp.php
@@ -1,13 +1,19 @@
 <?php
 include('db.php');
-
-if(isset($_POST['phone'])){
+$sql="SELECT * FROM user WHERE phone='".$_POST['phone']."'";
+$result=mysqli_query($conn,$sql);
+$num=mysqli_num_rows($result);
+if($num==0){
+    echo "Your phone number not registered. Please register first.";
+    exit;
+}else{
+    if(isset($_POST['phone'])){
     $phone = $_POST['phone'];
-    $otp = rand(100000, 999999); // 6-digit OTP
+    $otp = rand(100000, 999999); 
     $expiry = date("Y-m-d H:i:s", strtotime("+5 minutes"));
 
     
-    mysqli_query($conn, "DELETE FROM user_otp WHERE phone='$phone'"); // remove old OTP
+    mysqli_query($conn, "DELETE FROM user_otp WHERE phone='$phone'"); 
     $sql = "INSERT INTO user_otp (phone, otp, expiry_time) VALUES ('$phone', '$otp', '$expiry')";
     $result = mysqli_query($conn, $sql);
 
@@ -18,4 +24,6 @@ if(isset($_POST['phone'])){
         echo "Failed to generate OTP";
     }
 }
+}
+
 ?>
